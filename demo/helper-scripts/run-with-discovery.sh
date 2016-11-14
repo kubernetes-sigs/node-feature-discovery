0d31ad8f0ebd656f749695f722bd75ef3317fe3b
Balaji Subramaniam <balaji.subramaniam@intel.com>
2016-11-14 15:05:45 -0800

    Updated demo. - Updated pod templates with new label prefix. - Minor changes to job scripts and readme.

--- demo/helper-scripts/run-with-discovery.sh
+++ demo/helper-scripts/run-with-discovery.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 show_help() { 
 cat << EOF
-    Usage: ${0##*/} [-v DISCOVERY_VERSION] [-a APPLICATION_NAME]
+    Usage: ${0##*/} [-a APPLICATION_NAME]
     Runs pods ten times with discovery enabled.
 
-    -v DISCOVERY_VERSION    target discovery version DISCOVERY_VERSION.          
     -a APPLICATION_NAME     run the pods with APPLICATION_NAME application.
                             APPLICATION_NAME can be one of parsec or cloverleaf.
 EOF
@@ -16,17 +15,13 @@ then
     exit 1
 fi
 
-version="0.1.0"
 app="parsec"
 
 OPTIND=1
-options="hv:a:"
+options="ha:"
 while getopts $options option
 do
     case $option in 
-        v) 
-            version=$OPTARG
-            ;;
         a)
             if [ "$OPTARG" == "parsec" ] || [ "$OPTARG" == "cloverleaf" ]
             then
@@ -48,16 +43,16 @@ do
    esac
 done
 
-echo "Using discovery verion = $version and application name = $app."
+echo "Using application name = $app."
 echo "Creating pods with node feature discovery enabled."
 for i in {1..10}
 do
     if [ "$app" == "parsec" ]
     then
-        sed -e "s/NUM/$i-with-discovery/" -e "s/VER/$version/" -e "s/APP/demo-1/" demo-pod-with-discovery.json.parsec.template > demo-pod-with-discovery.json
+        sed -e "s/NUM/$i-with-discovery/" -e "s/APP/demo-1/" demo-pod-with-discovery.json.parsec.template > demo-pod-with-discovery.json
         kubectl create -f demo-pod-with-discovery.json
     else
-        sed -e "s/NUM/$i-with-discovery/" -e "s/VER/$version/" -e "s/APP/demo-2/" demo-pod-with-discovery.yaml.cloverleaf.template > demo-pod-with-discovery.yaml
+        sed -e "s/NUM/$i-with-discovery/" -e "s/APP/demo-2/" demo-pod-with-discovery.yaml.cloverleaf.template > demo-pod-with-discovery.yaml
         kubectl create -f demo-pod-with-discovery.yaml
     fi
     echo "WithDiscovery" >> labels-with-discovery-$app.log
