kubernetes-sigs/node-feature-discovery

commit 0ea8ff631e5b30e0236d96e3f284e15b924312bc

refactor sources to source pkg

source/panic_fake/fake_panic.go
@@ -1,0 +1,26 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package panic_fake
+
+// Source implements FeatureSource.
+type Source struct{}
+
+func (s Source) Name() string { return "panic_fake" }
+
+func (s Source) Discover() ([]string, error) {
+	panic("panic fake error")
+}

main.go
@@ -7,10 +7,16 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/docopt/docopt-go"
+	"github.com/kubernetes-incubator/node-feature-discovery/source"
+	"github.com/kubernetes-incubator/node-feature-discovery/source/cpuid"
+	"github.com/kubernetes-incubator/node-feature-discovery/source/fake"
+	"github.com/kubernetes-incubator/node-feature-discovery/source/panic_fake"
+	"github.com/kubernetes-incubator/node-feature-discovery/source/pstate"
+	"github.com/kubernetes-incubator/node-feature-discovery/source/rdt"
 	k8sclient "k8s.io/client-go/kubernetes"
 	api "k8s.io/client-go/pkg/api/v1"
 	restclient "k8s.io/client-go/rest"
+	"github.com/docopt/docopt-go"
 )
 
 const (
@@ -128,21 +134,22 @@ func argsParse(argv []string) (noPublish bool, sourcesArg []string, whiteListArg
 
 // configureParameters returns all the variables required to perform feature
 // discovery based on command line arguments.
-func configureParameters(sourcesArg []string, whiteListArg string) (sources []FeatureSource, labelWhiteList *regexp.Regexp, err error) {
+func configureParameters(sourcesArg []string, whiteListArg string) (sources []source.FeatureSource, labelWhiteList *regexp.Regexp, err error) {
 	enabledSources := map[string]struct{}{}
 	for _, s := range sourcesArg {
 		enabledSources[strings.TrimSpace(s)] = struct{}{}
 	}
 
 	// Configure feature sources.
-	allSources := []FeatureSource{
-		cpuidSource{},
-		rdtSource{},
-		pstateSource{},
-		fakeSource{},
+	allSources := []source.FeatureSource{
+		cpuid.Source{},
+		rdt.Source{},
+		pstate.Source{},
+		fake.Source{},
+		panic_fake.Source{},
 	}
 
-	sources = []FeatureSource{}
+	sources = []source.FeatureSource{}
 	for _, s := range allSources {
 		if _, enabled := enabledSources[s.Name()]; enabled {
 			sources = append(sources, s)
@@ -161,7 +168,7 @@ func configureParameters(sourcesArg []string, whiteListArg string) (sources []Fe
 
 // createFeatureLabels returns the set of feature labels from the enabled
 // sources and the whitelist argument.
-func createFeatureLabels(sources []FeatureSource, labelWhiteList *regexp.Regexp) (labels Labels) {
+func createFeatureLabels(sources []source.FeatureSource, labelWhiteList *regexp.Regexp) (labels Labels) {
 	labels = Labels{}
 	// Add the version of this discovery code as a node label
 	versionLabel := fmt.Sprintf("%s/%s.version", Namespace, ProgramName)
@@ -208,7 +215,7 @@ func updateNodeWithFeatureLabels(helper APIHelpers, noPublish bool, labels Label
 
 // getFeatureLabels returns node labels for features discovered by the
 // supplied source.
-func getFeatureLabels(source FeatureSource) (labels Labels, err error) {
+func getFeatureLabels(source source.FeatureSource) (labels Labels, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			stderrLogger.Printf("panic occurred during discovery of source [%s]: %v", source.Name(), r)

main_test.go
@@ -5,6 +5,9 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/kubernetes-incubator/node-feature-discovery/source"
+	"github.com/kubernetes-incubator/node-feature-discovery/source/fake"
+	"github.com/kubernetes-incubator/node-feature-discovery/source/panic_fake"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/vektra/errors"
 	k8sclient "k8s.io/client-go/kubernetes"
@@ -20,7 +23,7 @@ func TestDiscoveryWithMockSources(t *testing.T) {
 		for _, f := range fakeFeatures {
 			fakeFeatureLabels[fmt.Sprintf("%s-testSource-%s", prefix, f)] = "true"
 		}
-		fakeFeatureSource := FeatureSource(mockFeatureSource)
+		fakeFeatureSource := source.FeatureSource(mockFeatureSource)
 
 		Convey("When I successfully get the labels from the mock source", func() {
 			mockFeatureSource.On("Name").Return(fakeFeatureSourceName)
@@ -194,7 +197,7 @@ func TestConfigureParameters(t *testing.T) {
 			})
 			Convey("Proper sources are returned", func() {
 				So(len(sources), ShouldEqual, 1)
-				So(sources[0], ShouldHaveSameTypeAs, fakeSource{})
+				So(sources[0], ShouldHaveSameTypeAs, fake.Source{})
 				So(labelWhiteList, ShouldResemble, emptyRegexp)
 			})
 		})
@@ -232,8 +235,8 @@ func TestCreateFeatureLabels(t *testing.T) {
 	Convey("When creating feature labels from the configured sources", t, func() {
 		Convey("When fake feature source is configured", func() {
 			emptyLabelWL, _ := regexp.Compile("")
-			fakeFeatureSource := FeatureSource(new(fakeSource))
-			sources := []FeatureSource{}
+			fakeFeatureSource := source.FeatureSource(new(fake.Source))
+			sources := []source.FeatureSource{}
 			sources = append(sources, fakeFeatureSource)
 			labels := createFeatureLabels(sources, emptyLabelWL)
 
@@ -246,8 +249,8 @@ func TestCreateFeatureLabels(t *testing.T) {
 		})
 		Convey("When fake feature source is configured with a whitelist that doesn't match", func() {
 			emptyLabelWL, _ := regexp.Compile(".*rdt.*")
-			fakeFeatureSource := FeatureSource(new(fakeSource))
-			sources := []FeatureSource{}
+			fakeFeatureSource := source.FeatureSource(new(fake.Source))
+			sources := []source.FeatureSource{}
 			sources = append(sources, fakeFeatureSource)
 			labels := createFeatureLabels(sources, emptyLabelWL)
 
@@ -326,7 +329,7 @@ func TestRemoveLabels(t *testing.T) {
 
 func TestGetFeatureLabels(t *testing.T) {
 	Convey("When I get feature labels and panic occurs during discovery of a feature source", t, func() {
-		fakePanicFeatureSource := FeatureSource(new(fakePanicSource))
+		fakePanicFeatureSource := source.FeatureSource(new(panic_fake.Source))
 
 		returnedLabels, err := getFeatureLabels(fakePanicFeatureSource)
 		Convey("No label is returned", func() {

source/cpuid/cpuid.go
@@ -1,0 +1,30 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cpuid
+
+import "github.com/klauspost/cpuid"
+
+// Source implements FeatureSource.
+type Source struct{}
+
+func (s Source) Name() string { return "cpuid" }
+
+// Discover returns feature names for all the supported CPU features.
+func (s Source) Discover() ([]string, error) {
+	// Get the cpu features as strings
+	return cpuid.CPU.Features.Strings(), nil
+}

source/fake/fake.go
@@ -1,0 +1,31 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fake
+
+// Source implements FeatureSource.
+type Source struct{}
+
+func (s Source) Name() string { return "fake" }
+
+func (s Source) Discover() ([]string, error) {
+	features := []string{}
+
+	// Adding three fake features.
+	features = append(features, "fakefeature1", "fakefeature2", "fakefeature3")
+
+	return features, nil
+}

source/pstate/pstate.go
@@ -1,0 +1,44 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pstate
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+// Source implements FeatureSource.
+type Source struct{}
+
+func (s Source) Name() string { return "pstate" }
+
+// Discover returns feature names for p-state related features such as turbo boost.
+func (s Source) Discover() ([]string, error) {
+	features := []string{}
+
+	// Only looking for turbo boost for now...
+	bytes, err := ioutil.ReadFile("/sys/devices/system/cpu/intel_pstate/no_turbo")
+	if err != nil {
+		return nil, fmt.Errorf("can't detect whether turbo boost is enabled: %s", err.Error())
+	}
+	if bytes[0] == byte('0') {
+		// Turbo boost is enabled.
+		features = append(features, "turbo")
+	}
+
+	return features, nil
+}

source/rdt/rdt.go
@@ -1,0 +1,65 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rdt
+
+import (
+	"os/exec"
+	"path"
+
+	"github.com/golang/glog"
+)
+
+const (
+	// RDTBin is the path to RDT detection helpers.
+	RDTBin = "/go/src/github.com/kubernetes-incubator/node-feature-discovery/rdt-discovery"
+)
+
+// Source implements FeatureSource.
+type Source struct{}
+
+func (s Source) Name() string { return "rdt" }
+
+// Returns feature names for CMT and CAT if suppported.
+func (s Source) Discover() ([]string, error) {
+	features := []string{}
+
+	cmd := exec.Command("bash", "-c", path.Join(RDTBin, "mon-discovery"))
+	if err := cmd.Run(); err != nil {
+		glog.Errorf("support for RDT monitoring was not detected: %v", err)
+	} else {
+		// RDT monitoring detected.
+		features = append(features, "RDTMON")
+	}
+
+	cmd = exec.Command("bash", "-c", path.Join(RDTBin, "l3-alloc-discovery"))
+	if err := cmd.Run(); err != nil {
+		glog.Errorf("support for RDT L3 allocation was not detected: %v", err)
+	} else {
+		// RDT L3 cache allocation detected.
+		features = append(features, "RDTL3CA")
+	}
+
+	cmd = exec.Command("bash", "-c", path.Join(RDTBin, "l2-alloc-discovery"))
+	if err := cmd.Run(); err != nil {
+		glog.Errorf("support for RDT L2 allocation was not detected: %v", err)
+	} else {
+		// RDT L2 cache allocation detected.
+		features = append(features, "RDTL2CA")
+	}
+
+	return features, nil
+}

source/source.go
@@ -1,0 +1,26 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package source
+
+// FeatureSource represents a source of a discovered node feature.
+type FeatureSource interface {
+	// Name returns a friendly name for this source of node feature.
+	Name() string
+
+	// Discover returns discovered features for this node.
+	Discover() ([]string, error)
+}

commit fec3f5c64db6f9516ee7104c016daa121d69f26c

Fix typo in test mock code
A typo accidentally introduced in code refactoring in
0ea8ff631e5b30e0236d96e3f284e15b924312bc

source/panic_fake/fake_panic.go
@@ -22,5 +22,5 @@ type Source struct{}
 func (s Source) Name() string { return "panic_fake" }
 
 func (s Source) Discover() ([]string, error) {
-	panic("panic fake error")
+	panic("fake panic error")
 }