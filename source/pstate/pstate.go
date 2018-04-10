db929b878b26a0ebc8a8b2e74be9e6bcf87a8b2e
Bin Lu <bin.lu@arm.com>
2018-04-10 14:07:09 +0800

    Skip pstate detection on Arm
    Signed-off-by: Bin Lu <bin.lu@arm.com>

--- source/pstate/pstate.go
+++ source/pstate/pstate.go
@@ -19,6 +19,7 @@ package pstate
 import (
 	"fmt"
 	"io/ioutil"
+	"runtime"
 )
 
 // Source implements FeatureSource.
@@ -30,6 +31,13 @@ func (s Source) Name() string { return "pstate" }
 // Discover returns feature names for p-state related features such as turbo boost.
 func (s Source) Discover() ([]string, error) {
 	features := []string{}
+
+	// On Arm platform, the frequency boost mechanism is software-based.
+	// So skip pstate detection on Arm.
+	switch runtime.GOARCH {
+	case "arm64":
+		return features, nil
+	}
 
 	// Only looking for turbo boost for now...
 	bytes, err := ioutil.ReadFile("/sys/devices/system/cpu/intel_pstate/no_turbo")
