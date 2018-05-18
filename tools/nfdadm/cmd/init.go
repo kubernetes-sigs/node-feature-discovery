d8701c1273adf3e8051ef37c7941be6483a6550b
Markus Lehtonen <markus.lehtonen@intel.com>
2018-05-18 11:21:31 +0300

    nfdadm: implement init command

---
+++ tools/nfdadm/cmd/init.go
@@ -0,0 +1,144 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"os/user"
+	"path/filepath"
+
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type InitCmdFlags struct {
+	image      string
+	kubeconfig string
+}
+
+var initCmdFlags = &InitCmdFlags{}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	initCmd.Flags().StringVarP(&initCmdFlags.image, "image", "i", "quay.io/kubernetes_incubator/node-feature-discovery:v0.1.0", "Image to use for the node-feature-discovery binary")
+	initCmd.Flags().StringVarP(&initCmdFlags.kubeconfig, "kubeconfig", "c", defaultKubeconfig(), "Kubeconfig file to use for communicating with the API server")
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize NFD",
+	Long:  "Initialize Node Feature Discovery on a Kubernetes cluster",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Read kubeconfig
+		config, err := clientcmd.BuildConfigFromFlags("", initCmdFlags.kubeconfig)
+		if err != nil {
+			glog.Exitf("failed to read kubeconfig: %v", err)
+		}
+
+		// Create the client interface
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			glog.Exitf("failed to create clientset: %v", err)
+		}
+
+		// Configure DaemonSet
+		glog.Info("creating DaemonSet object for Node Feature Discovery")
+		_, err = createDaemonSet(initCmdFlags, clientset)
+		if err != nil {
+			glog.Exitf("failed to create DaemonSet: %v", err)
+		}
+	},
+}
+
+func createDaemonSet(flags *InitCmdFlags, clientset kubernetes.Interface) (*appsv1.DaemonSet, error) {
+
+	ds := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node-feature-discovery",
+			Namespace: "default",
+			Labels: map[string]string{
+				"app": "node-feature-discovery",
+			},
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "node-feature-discovery",
+				},
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "node-feature-discovery",
+					},
+				},
+				Spec: v1.PodSpec{
+					Volumes: []v1.Volume{
+						{
+							Name: "host-sys",
+							VolumeSource: v1.VolumeSource{
+								HostPath: &v1.HostPathVolumeSource{
+									Path: "/sys",
+								},
+							},
+						},
+					},
+					Containers: []v1.Container{
+						{
+							Name:  "node-feature-discovery",
+							Image: flags.image,
+							Args:  []string{"--sleep-interval=60s"},
+							Env: []v1.EnvVar{
+								{
+									Name: "NODE_NAME",
+									ValueFrom: &v1.EnvVarSource{
+										FieldRef: &v1.ObjectFieldSelector{
+											FieldPath: "spec.nodeName",
+										},
+									},
+								},
+							},
+							VolumeMounts: []v1.VolumeMount{
+								{
+									Name:      "host-sys",
+									ReadOnly:  true,
+									MountPath: "/host-sys",
+								},
+							},
+						},
+					},
+					HostNetwork: true,
+				},
+			},
+		},
+	}
+
+	return clientset.AppsV1().DaemonSets("default").Create(ds)
+}
+
+func defaultKubeconfig() string {
+	usr, err := user.Current()
+	if err != nil {
+		return ""
+	}
+	return filepath.Join(usr.HomeDir, ".kube", "config")
+}
