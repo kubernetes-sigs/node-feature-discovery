35d26001e4c957ba306000f782613673ab7339e5
Markus Lehtonen <markus.lehtonen@intel.com>
2019-02-11 15:10:29 +0200

    nfd-worker: extend unit test to cover 'main'
    Also, adds new method WaitForReady() into NfdMaster.
    
    In practice, this quite widely tests nfd-master, too, as the tests
    create an instance of NfdMaster and verify that the communication
    between master and worker works.

--- pkg/nfd-worker/nfd-worker_test.go
+++ pkg/nfd-worker/nfd-worker_test.go
@@ -17,11 +17,56 @@ limitations under the License.
 package nfdworker_test
 
 import (
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
+	nfdmaster "sigs.k8s.io/node-feature-discovery/pkg/nfd-master"
 	w "sigs.k8s.io/node-feature-discovery/pkg/nfd-worker"
+	"sigs.k8s.io/node-feature-discovery/test/data"
 )
+
+type testContext struct {
+	master nfdmaster.NfdMaster
+	errs   chan error
+}
+
+func setupTest(args nfdmaster.Args) testContext {
+	// Fixed port and no-publish, for convenience
+	args.NoPublish = true
+	args.Port = 8192
+	m, err := nfdmaster.NewNfdMaster(args)
+	if err != nil {
+		fmt.Printf("Test setup failed: %v\n", err)
+		os.Exit(1)
+	}
+	ctx := testContext{master: m, errs: make(chan error)}
+
+	// Run nfd-master instance, intended to be used as the server counterpart
+	go func() {
+		ctx.errs <- ctx.master.Run()
+		close(ctx.errs)
+	}()
+	ready := ctx.master.WaitForReady(time.Second)
+	if !ready {
+		fmt.Println("Test setup failed: timeout while waiting for nfd-master")
+		os.Exit(1)
+	}
+
+	return ctx
+}
+
+func teardownTest(ctx testContext) {
+	ctx.master.Stop()
+	for e := range ctx.errs {
+		if e != nil {
+			fmt.Printf("Error in test context: %v\n", e)
+			os.Exit(1)
+		}
+	}
+}
 
 func TestNewNfdWorker(t *testing.T) {
 	Convey("When initializing new NfdWorker instance", t, func() {
@@ -33,6 +78,48 @@ func TestNewNfdWorker(t *testing.T) {
 				So(err, ShouldNotBeNil)
 				So(err2, ShouldNotBeNil)
 				So(err3, ShouldNotBeNil)
+			})
+		})
+	})
+}
+
+func TestRun(t *testing.T) {
+	ctx := setupTest(nfdmaster.Args{})
+	defer teardownTest(ctx)
+	Convey("When running nfd-worker against nfd-master", t, func() {
+		Convey("When publishing features from fake source", func() {
+			worker, _ := w.NewNfdWorker(w.Args{Oneshot: true, Sources: []string{"fake"}, Server: "localhost:8192"})
+			err := worker.Run()
+			Convey("No error should be returned", func() {
+				So(err, ShouldBeNil)
+			})
+		})
+	})
+}
+
+func TestRunTls(t *testing.T) {
+	masterArgs := nfdmaster.Args{
+		CaFile:         data.FilePath("ca.crt"),
+		CertFile:       data.FilePath("nfd-test-master.crt"),
+		KeyFile:        data.FilePath("nfd-test-master.key"),
+		VerifyNodeName: false,
+	}
+	ctx := setupTest(masterArgs)
+	defer teardownTest(ctx)
+	Convey("When running nfd-worker against nfd-master with mutual TLS auth enabled", t, func() {
+		Convey("When publishing features from fake source", func() {
+			workerArgs := w.Args{
+				CaFile:             data.FilePath("ca.crt"),
+				CertFile:           data.FilePath("nfd-test-worker.crt"),
+				KeyFile:            data.FilePath("nfd-test-worker.key"),
+				Oneshot:            true,
+				Sources:            []string{"fake"},
+				Server:             "localhost:8192",
+				ServerNameOverride: "nfd-test-master"}
+			worker, _ := w.NewNfdWorker(workerArgs)
+			err := worker.Run()
+			Convey("No error should be returned", func() {
+				So(err, ShouldBeNil)
 			})
 		})
 	})
