7f43a3db4e02cedd1c0c85e0c41d2e02faeb7fab
Markus Lehtonen <markus.lehtonen@intel.com>
2019-05-07 12:41:20 +0300

    nfd-master: fix --label-whitelist
    Make the --label-whitelist effective. Previously, it was unused and had
    no effect. Also, add simple unit test for that.

--- pkg/nfd-worker/nfd-worker_test.go
+++ pkg/nfd-worker/nfd-worker_test.go
@@ -19,6 +19,7 @@ package nfdworker_test
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"testing"
 	"time"
 
@@ -37,6 +38,7 @@ func setupTest(args nfdmaster.Args) testContext {
 	// Fixed port and no-publish, for convenience
 	args.NoPublish = true
 	args.Port = 8192
+	args.LabelWhiteList = regexp.MustCompile("")
 	m, err := nfdmaster.NewNfdMaster(args)
 	if err != nil {
 		fmt.Printf("Test setup failed: %v\n", err)
