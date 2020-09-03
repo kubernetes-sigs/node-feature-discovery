4899de7e595cb7f066dac63778db98e3a6830b64
Markus Lehtonen <markus.lehtonen@intel.com>
2020-09-03 17:00:10 +0300

    scripts: use a reproducible version in push-image.sh
    We don't have a full git repo in the gcb builds so our version/tag magic
    inside the makefile does not work. With this patch we get the version
    information from the builder environment - fortunately we can do that by
    simply stripping the date prefix from the _GIT_TAG variable. By
    stripping the date of we match what our makefile does.

--- scripts/test-infra/push-image.sh
+++ scripts/test-infra/push-image.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
 
+# Strip 'vYYYYMMDD-' from the variable in order to get a reproducible
+# version and container image tag
+if [ -n "$_GIT_TAG" ]; then
+    export VERSION=${_GIT_TAG:10}
+fi
+
 make image -e
 make push -e
