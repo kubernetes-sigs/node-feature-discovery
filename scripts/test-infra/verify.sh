5974b4d905fb631e159e5067b5a3ad1e998ba94f
Markus Lehtonen <markus.lehtonen@intel.com>
2020-09-03 15:02:35 +0300

    scripts: fix verify.sh
    Modify PATH in order to make sure that golangci-lint is found.

--- scripts/test-infra/verify.sh
+++ scripts/test-infra/verify.sh
@@ -2,6 +2,7 @@
 
 # Install deps
 curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b $(go env GOPATH)/bin v1.27.0
+export PATH=$PATH:$(go env GOPATH)/bin
 
 # Run verify steps
 make gofmt-verify -e
