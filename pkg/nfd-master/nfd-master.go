f64c23968ad21dc855eb05fe46279e1b05c75ea4
Markus Lehtonen <markus.lehtonen@intel.com>
2023-04-06 21:20:24 +0300

    nfd-master: fix node update
    Update node status before node metadata. This fixes a problem where we
    lose track of NFD-managed extended resources in case patching node
    status fails. Previously we removed all labels and annotations
    (including the one listing our ERs) and only after that updated node
    status. If node status update failed we had lost the annotation but
    extended resources were still there, leaving them orphaned.

--- pkg/nfd-master/nfd-master.go
+++ pkg/nfd-master/nfd-master.go
@@ -878,17 +878,17 @@ func (m *nfdMaster) updateNodeObject(cli *kubernetes.Clientset, nodeName string,
 	patches := createPatches(oldLabels, node.Labels, labels, "/metadata/labels")
 	patches = append(patches, createPatches(nil, node.Annotations, annotations, "/metadata/annotations")...)
 
-	// Patch the node object in the apiserver
-	err = m.apihelper.PatchNode(cli, node.Name, patches)
-	if err != nil {
-		return fmt.Errorf("error while patching node object: %v", err)
-	}
-
 	// patch node status with extended resource changes
 	statusPatches := m.createExtendedResourcePatches(node, extendedResources)
 	err = m.apihelper.PatchNodeStatus(cli, node.Name, statusPatches)
 	if err != nil {
 		return fmt.Errorf("error while patching extended resources: %v", err)
+	}
+
+	// Patch the node object in the apiserver
+	err = m.apihelper.PatchNode(cli, node.Name, patches)
+	if err != nil {
+		return fmt.Errorf("error while patching node object: %v", err)
 	}
 
 	if len(patches) > 0 || len(statusPatches) > 0 {
