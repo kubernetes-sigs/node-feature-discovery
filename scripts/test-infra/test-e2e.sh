23fda760700a0e8a5ed3fae6159b8497e3035c3d
Markus Lehtonen <markus.lehtonen@intel.com>
2024-07-08 13:38:38 +0300

    scripts/test-infra: run postsubmit e2e test in kind

--- scripts/test-infra/test-e2e.sh
+++ scripts/test-infra/test-e2e.sh
@@ -1,21 +1,12 @@
 #!/bin/bash -e
 
-# Install deps
-curl -o /usr/local/bin/aws-iam-authenticator -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.7/aws-iam-authenticator_0.5.7_linux_amd64
-chmod a+x /usr/local/bin/aws-iam-authenticator
-
-curl -o /usr/local/bin/kubectl -L https://dl.k8s.io/release/v1.24.0/bin/linux/amd64/kubectl
-chmod a+x /usr/local/bin/kubectl
-
-
 # Configure environment
+export KIND_VERSION="v0.23.0"
+export KIND_NODE_IMAGE="kindest/node:v1.30.2"
+export CLUSTER_NAME="nfd-e2e"
 export KUBECONFIG=`pwd`/kubeconfig
-export E2E_TEST_CONFIG=`pwd`/e2e-test-config
+export IMAGE_REGISTRY="gcr.io/k8s-staging-nfd"
 export E2E_TEST_FULL_IMAGE=true
-
-echo "$KUBECONFIG_DATA" > "$KUBECONFIG"
-echo "$E2E_TEST_CONFIG_DATA" > "$E2E_TEST_CONFIG"
-
 
 # Wait for the image to be built and published
 i=1
