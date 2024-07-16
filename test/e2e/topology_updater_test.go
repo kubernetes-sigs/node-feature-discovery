5aeea2895770f8dd7fd153d13985a9227ba76fa5
Markus Lehtonen <markus.lehtonen@intel.com>
2024-07-16 12:28:34 +0300

    test/e2e: specify -sleep-interval in topology-updater exclude-memory test
    Make the test finish considerably faster.

--- test/e2e/topology_updater_test.go
+++ test/e2e/topology_updater_test.go
@@ -402,6 +402,7 @@ excludeList:
 
 			podSpecOpts := []testpod.SpecOption{
 				testpod.SpecWithContainerImage(dockerImage()),
+				testpod.SpecWithContainerExtraArgs("-sleep-interval=5s"),
 				testpod.SpecWithConfigMap(cm.Name, "/etc/kubernetes/node-feature-discovery"),
 			}
 			topologyUpdaterDaemonSet = testds.NFDTopologyUpdater(kcfg, podSpecOpts...)
