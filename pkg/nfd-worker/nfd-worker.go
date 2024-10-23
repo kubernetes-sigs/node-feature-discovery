4ba35eb8ceca7b7898006cad1141bfefe0d4d793
Markus Lehtonen <markus.lehtonen@intel.com>
2024-10-23 15:19:53 +0300

    nfd-worker: add healthz endpoint

--- pkg/nfd-worker/nfd-worker.go
+++ pkg/nfd-worker/nfd-worker.go
@@ -200,6 +200,10 @@ func newDefaultConfig() *NFDConfig {
 	}
 }
 
+func (w *nfdWorker) Healthz(writer http.ResponseWriter, _ *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+}
+
 func (i *infiniteTicker) Reset(d time.Duration) {
 	switch {
 	case d > 0:
@@ -304,7 +308,8 @@ func (w *nfdWorker) Run() error {
 		return nil
 	}
 
-	// Start readiness probe (at this point we're "ready and live")
+	// Register health probe (at this point we're "ready and live")
+	httpMux.HandleFunc("/healthz", w.Healthz)
 
 	// Start HTTP server
 	httpServer := http.Server{Addr: fmt.Sprintf(":%d", w.args.Port), Handler: httpMux}
