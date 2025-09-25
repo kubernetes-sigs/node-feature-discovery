d3cdebae76bdd5c7f820b2f99a1a9fbab66bad6a
Markus Lehtonen <markus.lehtonen@intel.com>
2025-09-25 14:07:55 +0300

    Clean up cloudbuild
    Don't specify the IMAGE_EXTRA_TAG_NAMES when we're building release (git
    tag) images.

--- scripts/test-infra/push-image.sh
+++ scripts/test-infra/push-image.sh
@@ -3,14 +3,18 @@
 # Override VERSION if _GIT_TAG is specified. Strip 10 first characters
 # ('vYYYYMMDD-') from _GIT_TAG in order to get a reproducible version and
 # container image tag
-VERSION_OVERRIDE=${_GIT_TAG+VERSION=${_GIT_TAG:10}}
+if [ -z "$_GIT_TAG" ]; then
+    MAKE_VARS="IMAGE_EXTRA_TAG_NAMES=${_PULL_BASE_REF}"
+else
+    MAKE_VARS="VERSION=${_GIT_TAG:10}"
+fi
 
 # Authenticate in order to be able to push images
 gcloud auth configure-docker
 
 # Build and push images
-IMAGE_ALL_PLATFORMS=linux/amd64,linux/arm64 make push-all $VERSION_OVERRIDE
+IMAGE_ALL_PLATFORMS=linux/amd64,linux/arm64 make push-all $MAKE_VARS
 
 go install helm.sh/helm/v3/cmd/helm@v3.17.3
 
-make helm-push $VERSION_OVERRIDE
+make helm-push $VERSION_OVERRIDE $MAKE_VARS
