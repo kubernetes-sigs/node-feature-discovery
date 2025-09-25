1581211fd3c708a3cdcb8103669437be9e1fbdec
Markus Lehtonen <markus.lehtonen@intel.com>
2025-09-25 16:16:02 +0300

    cloudbuild: fix build
    Bump cloudbuild image to the latest version and match oras version with
    the golang version within.

--- scripts/test-infra/push-image.sh
+++ scripts/test-infra/push-image.sh
@@ -16,6 +16,6 @@ gcloud auth configure-docker
 IMAGE_ALL_PLATFORMS=linux/amd64,linux/arm64 make push-all $MAKE_VARS
 
 go install helm.sh/helm/v3/cmd/helm@v3.17.3
-go install oras.land/oras/cmd/oras@v1.3.0
+go install oras.land/oras/cmd/oras@v1.2.3
 
 make helm-push $VERSION_OVERRIDE $MAKE_VARS
